f45a357f1bf552f032b9462664f33bcb96a04d4e
qwg4fh6qqc-code <qwg4fh6qqc@privaterelay.appleid.com>
2026-09-04 17:32:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ let clicks = 0;
 let started = false;
 
 const phrases = [
-  "Toca el corazón Marcela ❤️",
-  "Ufff casi pero te falta... ❤️",
-  "Ya casiiiii solo dale un poquito mas... ❤️",
+  "Toca el corazón ❤️",
+  "Ufff casi, pero te falta... ❤️",
+  "Ya casiiiii, solo dale un poquito mas... ❤️",
   "Un último toque, a veces lo bueno se hace esperar... ❤️",
   "¿Estás lista, Marce? Porque yo tengo nervios... ❤️"
 ];
